71eb92631d282ee233c8fb38c89a1ffd6bc5a197
Sean Ransonette <sransonette@gmail.com>
2020-08-05 02:37:28 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,34 @@
+require 'pry'
+
+
 class Transfer
-  # your code here
+  
+  
+  attr_accessor :sender, :receiver, :status, :amount
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+    
+  end
+  
+  def valid? 
+    sender.valid? && receiver.valid?
+    
+  end
+  
+  def execute_transaction
+    if valid? && sender.balance > amount && status == "pending"
+      from = @sender
+      to = @receiver
+     # binding.pry
+    else 
+      @staus
+      
+    end
+  end
+  
+  
 end
